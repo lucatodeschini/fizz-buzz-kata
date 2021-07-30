235387899297356c36172a411c676faf94caae9e
Luca Todeschini <luca.todeschini@ingka.ikea.com>
2021-07-30 15:35:11 +0200

    Using springboot project

---
+++ src/main/java/com/lucatode/fizzbuzzkata/FizzbuzzkataApplication.java
@@ -0,0 +1,13 @@
+package com.lucatode.fizzbuzzkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzkataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucatode/fizzbuzzkata/FizzbuzzkataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatode.fizzbuzzkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzkataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
